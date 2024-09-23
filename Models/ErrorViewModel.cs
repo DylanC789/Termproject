a91a2eaac04a8d0286ac520f7aab5868f1c1f24a
DylanC789 <craft13@mail.nmc.edu>
2024-09-23 00:44:14 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Termproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
